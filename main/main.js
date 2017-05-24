c481c9aa9e91a9f4d490a1d4d2a81390e24256cb
Candy <229355694@qq.com>
2017-05-24 11:33:07 +0800

    third commit

--- main/main.js
+++ main/main.js
@@ -1,8 +1,8 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
-  var index=email.indexOf("@");
-  var suffix=email.substring(index+1);
-  if(suffixes.indexOf(suffix)!=-1){
+  var index = email.indexOf("@");
+  var suffix = email.substring(index + 1);
+  if (suffixes.indexOf(suffix) != -1) {
     return true;
   }
   return false;
